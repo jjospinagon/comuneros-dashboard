5f02dbb19ce1111f5c9f47b024739ecb4607f697
Juan Jose Ospina <jjospinagon@gmail.com>
2026-07-17 16:44:04 -0500

    autosync win vie 17/07/2026 16:44:01.91

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Cockpit Comuneros · Service Worker v1
    - Network-first para recursos del mismo origen (siempre fresco; offline = último cacheado)
    - NUNCA intercepta script.google.com (JSONP) ni CDNs externas */
-const CACHE = 'cockpit-v4-1';
+const CACHE = 'cockpit-v4-2';
 const SHELL = ['./', './index.html', './v2.html', './manifest.json',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
